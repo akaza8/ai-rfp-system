4cbbb23a0a9c9db8049e01f28a3d457c1f796893
akash <akashlawande852@gmail.com>
2025-12-02 15:46:41 +0530

    initial setup with frontend and backend

---
+++ backend/src/test/java/com/ak/rfp/RfpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ak.rfp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RfpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
